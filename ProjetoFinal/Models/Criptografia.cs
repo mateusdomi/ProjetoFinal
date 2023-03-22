759d862d3d27a4fcf089511b12e3ff2307788a03
Lucas Damazio <lucas.damaziioo@gmail.com>
2023-03-21 21:35:01 -0300

    Model - Criptografia

---
+++ ProjetoFinal/Models/Criptografia.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ProjetoFinal.Models
+{
+    public class Criptografia
+    {
+        public string Criptografar(string texto)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public string CriptografarNomeUsuario(string nomeUsuario)
+        {
+            return Criptografar(nomeUsuario);
+        }
+
+        public string CriptografarSenha(string senha)
+        {
+            return Criptografar(senha);
+        }
+
+        public static bool CompararHashes(string hashString1, string hashString2)
+        {
+            byte[] hash1 = Convert.FromHexString(hashString1);
+            byte[] hash2 = Convert.FromHexString(hashString2);
+
+
+            if (hash1 == null || hash2 == null || hash1.Length != hash2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < hash1.Length; i++)
+            {
+                if (hash1[i] != hash2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
